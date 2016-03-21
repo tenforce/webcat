643b8248c380fcf8a8052cd91a68d9760f94c60a
Aad Versteden <madnificent@gmail.com>
2016-03-21 16:07:06 +0100

    Update console hints during release
    The console hints help you publish releases.  I hope these will help us
    sufficiently in the distant future.  Not perfect, but probably the
    cheapest solution in total.

--- config/release.js
+++ config/release.js
@@ -14,7 +14,7 @@ module.exports = {
   beforeCommit: function(project, tags) {
     return new Promise( function( success, failure ) {
       // build the latest sources
-      console.log("building latest");
+      console.log("Creating new production build...");
       var task = new BuildTask({
         project: project,
         ui: project.ui,
@@ -26,14 +26,14 @@ module.exports = {
         outputPath: 'dist/'
       }).then( function() {
         // zip the latest sources
-        console.log("creating new zip");
+        console.log("Zipping the sources...");
         var zip = new Zip();
         zip.addLocalFolder( path.join(project.root, "dist"), "dist" );
         zip.writeZip( path.join(project.root, "dist.zip") );
-        console.log('Wrote built project to dist.zip');
+        console.log('[TODO] Please upload ' + path.join(project.root, "dist.zip") + ' to the GitHub release once the tag is pushed and maybe trigger the build on hub.docker.com again.');
 
         // update version in docker-compose.yml
-        console.log("updating docker-compose.yml");
+        console.log("Updating docker-compose.yml with new version...");
         var composePath = path.join(project.root, 'docker-compose.yml');
         var nextDockerVersion = tags.next.slice(1);
         fs.readFile(composePath, 'utf8', function (err,data) {
