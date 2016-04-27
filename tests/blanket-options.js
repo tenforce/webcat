5f804b07fd6901e6af3c79564376001a4e5f2812
Cecile Tonglet <cecile.tonglet@gmail.com>
2016-04-27 12:19:52 +0200

    Enable codecov

---
+++ tests/blanket-options.js
@@ -0,0 +1,19 @@
+/* globals blanket, module */
+
+var options = {
+  branchTracking: true,
+  modulePrefix: 'webcat',
+  filter: '//.*webcat/.*/',
+  antifilter: '//.*(tests|template).*/',
+  loaderExclusions: [],
+  enableCoverage: true,
+  cliOptions: {
+    reporters: ['lcov'],
+    autostart: true
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
